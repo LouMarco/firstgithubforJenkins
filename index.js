edefc6ef058385b60b20baa7faffeb61e856eef3
Lou the instructor <loumarco@hotmail.com>
2019-09-16 15:10:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World! Changed!!! Project NOT IN git directory!!! and changed in github!!!'))
+app.get('/', (req, res) => res.send('Hello World! Changed!!! \nProject NOT IN git directory!!! \nand changed in github!!!\nand changed in ATOM on the desktop!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
